287e8e39f76b4c35b06ead0c20f48c54da007bc1
derreck <mansheim23@hotmail.com>
2016-05-11 16:40:57 -0500

    add movie scaffold and users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :movies
+
+  root 'movies#index'
+end
+
+
+
+
+
+
+
+
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +70,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
